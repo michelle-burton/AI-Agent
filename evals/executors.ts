5c012e2586850211c495fe44ef8c4beb35f95135
Michelle Burton <msmichelleburton@yahoo.com>
2026-01-19 13:21:44 -0600

    adding evals

--- evals/executors.ts
+++ evals/executors.ts
@@ -9,4 +9,23 @@ import type {
   MultiTurnEvalData,
   MultiTurnResult,
 } from "./types.ts";
+import { readFile } from "fs";
 
+const TOOL_DEFINITIONS = {
+    readFile: {
+        description: "Read the contents of a file at the specified path.",
+        parmeters: z.object({
+            path: z.string().describe("the path to the file that you want to read"),
+        })
+    },
+    writeFile: {
+        description: "Write given content to the file at the given path.",
+        parmeters: z.object({
+            path: z.string().describe("the path to the file that you want to write to"),
+            content: z.string().describe("the content you want to write to the file"),
+        })
+    },
+    listFile: {},
+    deleteFile: {},
+    runCommand: {},
+}
