36266e30d0231c3f74ce0ee7e0bae42703baf6e1
Michelle Burton <msmichelleburton@yahoo.com>
2026-01-20 09:01:20 -0600

    add system prompt object

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import { streamText, type ModelMessage } from 'ai';
-//import { generateText, stepCountIs, type ModelMessage } from 'ai'
 import { openai } from '@ai-sdk/openai';
 import { getTracer, Laminar } from '@lmnr-ai/lmnr';
 import { tools } from "./tools/index.ts";
@@ -9,46 +8,26 @@ import { SYSTEM_PROMPT } from './system/prompt.ts'
 import type { AgentCallbacks, ToolCallInfo } from '../types.ts';
 import { filterCompatibleMessages} from "./system/filterMessages.ts"
 
-
+Laminar.initialize({
+    projectApiKey: process.env.LMNR_PROJECT_API_KEY,
+})
 
 
 const MODEL_NAME = "gpt-5-mini";
 
-Laminar.initialize({
-    projectApiKey: process.env.LMNR_PROJECT_API_KEY,
-})
+
 
 export const runAgent = async (
     userMessage: string,
     conversationHistory: ModelMessage[],
     callbacks: AgentCallbacks,
-) => {
-    const { text, toolCalls } = await generateText({
-        model: openai(MODEL_NAME),
-        //prompt: userMessage,
-        messages: [],
-        system: SYSTEM_PROMPT,
-        //temperature: 0, // less randomness
-        tools,
-        // stopWhen: stepCountIs(2),
-        experimental_telemetry: {
-            isEnabled: true,
-            tracer: getTracer(),
-        }
-    });
-    
-    await Laminar.flush();
+): Promise<ModelMessage[]> {
+    const workingHistory = filterCompatibleMessages(conversationHistory):
 
-     console.log('done')
-    //console.log(text)
-    // toolCalls.forEach(async (tc) => {
-    //     const result = await executeTools(tc.toolName as any, tc.input)
-    //     console.log(result)
-    // })
-   // console.log(text, toolCalls)
-};
+    const messages: ModelMessage[] = [{ role: 'system', content: SYSTEM_PROMPT }]
+}
 
-// run in terminal: npx tsx src/agent/run.ts
-//runAgent("What is the current time?")
+
+
 
 
